8213fe3161faa00ec28eded13f822d5e51ae953d
Anna Lebiedziewicz <nalin.kane@gmail.com>
2020-02-28 19:19:40 +0000

    routing

---
+++ amj_l/src/components/Layout/index.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+function Layout({ children }) {
+  return (
+    <div>
+      <header>sup</header>
+      {children}
+      <footer>yo</footer>
+    </div>
+  );
+}
+
+export default Layout;

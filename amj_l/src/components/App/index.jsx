eca588bb7f5c8b5f4a0a9b2d5cd116f789952a23
Anna Lebiedziewicz <nalin.kane@gmail.com>
2020-02-28 19:42:07 +0000

    header and footer added

--- amj_l/src/components/App/index.jsx
+++ amj_l/src/components/App/index.jsx
@@ -1,33 +1,12 @@
 import React from "react";
-import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import Layout from "../Layout";
-import Home from "../../pages/Home";
-import About from "../../pages/About";
-
-const routes = [
-  {
-    path: "/",
-    exact: true,
-    component: Home
-  },
-  {
-    path: "/about",
-    component: About
-  }
-];
+import routes from "../../routes";
 
 function App() {
   return (
     <Router>
       <Layout>
-        <ul>
-          <li>
-            <Link to="/">Home</Link>
-          </li>
-          <li>
-            <Link to="/about">About</Link>
-          </li>
-        </ul>
         <Switch>
           {routes.map((route, i) => (
             <Route key={i} {...route} />

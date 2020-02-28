bb2d240f2b963729565e9a5152cbbe1c825e0d87
Anna Lebiedziewicz <nalin.kane@gmail.com>
2020-02-28 22:41:02 +0000

    Updates

---
+++ precache-manifest.dee1067568198274799f020ba58330ab.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "75987fbb36072387e00319dda2a53047",
+    "url": "/amj_l/index.html"
+  },
+  {
+    "revision": "8f5957c5a66de1593560",
+    "url": "/amj_l/static/css/main.e316f3b7.chunk.css"
+  },
+  {
+    "revision": "796eddb8838808a0a3ff",
+    "url": "/amj_l/static/js/2.b7fa8f4d.chunk.js"
+  },
+  {
+    "revision": "f231859d6585c4cd5f80c344783ed269",
+    "url": "/amj_l/static/js/2.b7fa8f4d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8f5957c5a66de1593560",
+    "url": "/amj_l/static/js/main.341a5973.chunk.js"
+  },
+  {
+    "revision": "bb3f7d136bdec6fa5604",
+    "url": "/amj_l/static/js/runtime-main.6b607ea9.js"
+  }
+]);
